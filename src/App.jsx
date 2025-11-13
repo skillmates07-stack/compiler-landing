02d74af6d8ec086ced50c95c43ea2b20931be9cb
skillmates07-stack <skillmates07@gmail.com>
2025-11-14 00:18:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import HeroSection from './components/sections/HeroSection';
 import FeaturesSection from './components/sections/FeaturesSection';
@@ -5,18 +6,31 @@ import DemoSection from './components/sections/DemoSection';
 import PricingSection from './components/sections/PricingSection';
 import CTASection from './components/sections/CTASection';
 import Footer from './components/layout/Footer';
+import SignIn from './pages/SignIn';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="min-h-screen bg-black">
-      <Header />
-      <HeroSection />
-      <FeaturesSection />
-      <DemoSection />
-      <PricingSection />
-      <CTASection />
-      <Footer />
-    </div>
+    <Router>
+      <Routes>
+        {/* Landing Page */}
+        <Route path="/" element={
+          <div style={{ background: '#000000' }}>
+            <Header />
+            <HeroSection />
+            <FeaturesSection />
+            <DemoSection />
+            <PricingSection />
+            <CTASection />
+            <Footer />
+          </div>
+        } />
+        
+        {/* Auth Pages */}
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
