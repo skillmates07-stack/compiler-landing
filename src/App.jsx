30418952c94a26b928a22d7900fd38d45a057578
skillmates07-stack <skillmates07@gmail.com>
2025-11-13 21:48:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/layout/Header';
 import HeroSection from './components/sections/HeroSection';
+import FeaturesSection from './components/sections/FeaturesSection';
 
 function App() {
   return (
     <div className="min-h-screen bg-black">
       <Header />
       <HeroSection />
+      <FeaturesSection />
     </div>
   );
 }
