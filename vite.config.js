55842808bd61843d52af4e44b024070b96f9daf6
skillmates07-stack <skillmates07@gmail.com>
2025-11-13 21:11:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
     cssCodeSplit: false,
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      }
-    }
-  },
-  css: {
-    postcss: './postcss.config.js'
   }
 })
