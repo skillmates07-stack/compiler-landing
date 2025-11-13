89094119c86b0d23c767ae238791b54505579f19
skillmates07-stack <skillmates07@gmail.com>
2025-11-13 20:09:28 +0530

    Update App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/layout/Header';
+import HeroSection from './components/sections/HeroSection';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-dark-bg">
+      <Header />
+      <HeroSection />
+      {/* More sections will be added */}
+    </div>
+  );
+}
+
+export default App;
